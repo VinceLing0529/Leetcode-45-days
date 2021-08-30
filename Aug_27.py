4d1777fe37d437f4b493368916fc32ec8057edd0
VinceLing0529 <ldkxnb@gmail.com>
2021-08-29 17:44:05 -0700

    27

--- Aug_27.py
+++ Aug_27.py
@@ -14,3 +14,47 @@ def isValidBST(self, root: Optional[TreeNode]) -> bool:
         if not (root.val < right and root.val > left) : return False
         return dfs(root.left, left,root.val) and dfs(root.right,root.val,right)
     return dfs(root,-2147483649,2**31)
+
+#208. Implement Trie (Prefix Tree)
+class Trie:
+
+def __init__(self):
+    """
+    Initialize your data structure here.
+    """
+    self.root = {}
+
+def insert(self, word: str) -> None:
+    """
+    Inserts a word into the trie.
+    """
+    root = self.root
+    for i in word:
+        if i not in root:
+            root[i] = {}
+        root = root[i]
+    root["end"] = 1
+    
+
+def search(self, word: str) -> bool:
+    """
+    Returns if the word is in the trie.
+    """
+    root = self.root
+    for i in word:
+        if i not in root:
+            return False
+        root = root[i]
+    return "end" in root
+
+def startsWith(self, prefix: str) -> bool:
+    """
+    Returns if there is any word in the trie that starts with the given prefix.
+    """
+    
+    root = self.root
+    for i in prefix:
+        if i not in root:
+            return False
+        root = root[i]
+    return True
